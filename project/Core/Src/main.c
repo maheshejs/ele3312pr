55ce13ab02b69ead814f315b2924ed9814b3cd19
Vincent Therrien <vincent.therrien@polymtl.ca>
2020-11-24 15:52:58 -0500

    affichge du filet et des scores Fonctions d'aide pour le USART Contrôle de la raquette

--- project/Core/Src/main.c
+++ project/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "dma.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -65,6 +66,20 @@
 
 extern volatile uint32_t pulse_width;
 volatile uint8_t done = 0;
+
+#define BufferSize 4
+uint8_t transferBuffer[BufferSize];
+uint8_t receiveBuffer[BufferSize];
+
+volatile bool flagTransmitted = false;
+volatile bool flagReceived = false;
+volatile uint32_t local_time = 0;
+
+#define isController
+//#define isExecuter
+
+uint8_t score_g;
+uint8_t score_d;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -173,6 +188,41 @@ void InitGame()
 	mPade2.ySpeed = 0;	
 }
 
+/** Transmit the already filled 'transferBuffer' array through
+* USART and wait until it is received.
+*/
+void finishTransmission(int nBytes){
+	HAL_UART_Transmit_DMA(&huart3, transferBuffer, nBytes);
+	while (flagTransmitted == false);
+	flagTransmitted = false;
+	local_time = 0;
+	while (flagReceived == false && local_time < 1000);
+	flagReceived = false;
+}
+#ifdef isController
+void sendPosition(){
+	transferBuffer[0] = 254;
+	transferBuffer[1] = (uint8_t) (mBall.xPos);
+	transferBuffer[2] = (uint8_t) (mBall.yPos / 2);
+	transferBuffer[3] = (uint8_t) (mPade1.xPos);
+	finishTransmission(4);
+}
+void sendScore(){
+	transferBuffer[0] = 255;
+	transferBuffer[1] = score_g;
+	transferBuffer[2] = score_d;
+	transferBuffer[3] = 0;
+	finishTransmission(4);
+}
+#endif
+#ifdef isExecuter
+void sendHeight(){
+	transferBuffer[0] = 253;
+	transferBuffer[1] = (uint8_t) (mPade1.xPos);
+	finishTransmission(2);
+}
+#endif
+
 void MoveBall()
 {
 	switch (CollisionBall())
@@ -196,10 +246,14 @@ void MoveBall()
 								break;
 	}
 	
-	LCD_FillCircle(mBall.xPos,mBall.yPos,RBALL,BLACK);
+	LCD_FillCircle(mBall.xPos,mBall.yPos,RBALL+2,BLACK);
 	mBall.xPos += mBall.xSpeed;
 	mBall.yPos += mBall.ySpeed;
-	LCD_FillCircle(mBall.xPos,mBall.yPos,RBALL,WHITE);
+	HAL_UART_Receive_DMA(&huart3, receiveBuffer, 4);
+	
+	sendPosition();
+	
+	LCD_FillCircle(receiveBuffer[1], receiveBuffer[2] * 2, RBALL, WHITE);
 }
 
 struct Tool MovePong(struct Tool mPong)
@@ -214,6 +268,24 @@ struct Tool MovePong(struct Tool mPong)
 	return mPong;
 }
 
+void drawNet(){
+	LCD_FillRect(FONT, 320/2 - FONT/2, 240-(2*FONT), FONT, BLACK);
+	for (int i = 0; i < 12; i += 2){
+		LCD_FillRect(FONT + (i*2*FONT), 320/2 - FONT/2, FONT*2, FONT, WHITE);
+	}
+}
+void placePongWithDistance(struct Tool* mPong, float distance){
+	// Remove 2 cm because it is too close to the sensor, normalise with 30 cm
+	// to define a working range and scale to screen width (240 - 20 px).
+	if (distance < 2.0)
+		distance = 2.0;
+	float normalisedDistance = ((distance - 2.0) / 30.0) * (float)(MAXHEIGHT - (2*FONT));
+	//  Check if the position is too large.
+	if (normalisedDistance + PONGSIZE > MAXHEIGHT - (2*FONT))
+		normalisedDistance = MAXHEIGHT - PONGSIZE - (2*FONT);
+	// Place pong
+	mPong->xPos = normalisedDistance + FONT;
+}
 
 /* USER CODE END 0 */
 
@@ -224,7 +296,6 @@ struct Tool MovePong(struct Tool mPong)
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -245,38 +316,54 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_TIM2_Init();
   MX_TIM3_Init();
+  MX_USART1_UART_Init();
+  MX_USART3_UART_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-		HAL_Delay(1000);
-		SetLCD();
-		InitGame();
-		BorderGame();	
-		HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin,GPIO_PIN_SET);
-		HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
-		HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
-		HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
+	HAL_Delay(1000);
+	SetLCD();
+	InitGame();
+	BorderGame();	
+	HAL_GPIO_WritePin(LD2_GPIO_Port,LD2_Pin,GPIO_PIN_SET);
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
+	HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+	float distance = 0;
   while (1)
   {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-		LCD_FillRect(100, 160, 130, 25, BLACK);
-		LCD_SetCursor(100, 160);
-		LCD_Printf("%i\r\n", (pulse_width)/58);
-		printf("%i\r\n", (pulse_width)/58);
+		drawNet();
+		// Display score
+		LCD_FillRect(50, 80, 130, 25, BLACK);
+		LCD_SetCursor(50, 80);
+		LCD_Printf("%i\r\n", score_d); // Player 1
+		LCD_FillRect(50, 240, 130, 25, BLACK);
+		LCD_SetCursor(50, 240);
+		LCD_Printf("%i\r\n", score_g); // Player 2
 		
-		mPade1 = MovePong(mPade1);
+		
+		// Measure distance
+		distance = ((float)pulse_width)/58.0;
+		
+		//mPade1 = MovePong(mPade1);
 		mPade2 = MovePong(mPade2);		
 		MoveBall();
 		
 		//emulate player one :
-		mPade1.xSpeed = (rand()%5-2);
+		//mPade1.xSpeed = (rand()%5-2);
+		LCD_FillRect(mPade1.xPos,mPade1.yPos,PONGSIZE,FONT,BLACK);
+		placePongWithDistance(&mPade1, distance);
+		LCD_FillRect(mPade1.xPos,mPade1.yPos,PONGSIZE,FONT,WHITE);
 		
 		//emulate player two :
 		mPade2.xSpeed = (rand()%5-2);
@@ -346,15 +433,17 @@ PUTCHAR_PROTOTYPE
     HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, 0xFFFF);
     return ch;
 }
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	if (huart->Instance == USART3)
+		flagReceived = true;
+}
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart){
+	if (huart->Instance == USART3)
+		flagTransmitted = true;
+}
 
 void HAL_SYSTICK_Callback(void) {
-	static int local_time = 0;
 	local_time++;
-	if(local_time == 200)
-	{
-		done = 1;
-		local_time = 0;
-	}
 }
 
 /* USER CODE END 4 */
